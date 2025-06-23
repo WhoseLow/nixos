482d764446aa8a49e2b5bfe30917eed9b206cbda
WhoseLow <whose.low@protonmail.com>
2025-06-23 11:57:09 -0400

    Roll back updates

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -29,7 +29,7 @@
       proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
       # Open ports in the firewall.
-      # networking.firewall.allowedTCPPorts = [ ... ];
+      firewall.allowedTCPPorts = [25565];
       # networking.firewall.allowedUDPPorts = [ ... ];
       # Or disable the firewall altogether.
       # networking.firewall.enable = false;
